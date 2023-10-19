e83d38eb4baf27447a40b0d3515b61eb9edad353
alishersodiqov09 <alishersodiqov09@gmail.com>
2023-10-19 20:14:55 +0500

    yana bir commit

--- src/layouts/dashboard/data/reportsLineChartData.js
+++ src/layouts/dashboard/data/reportsLineChartData.js
@@ -1,10 +1,10 @@
 export default {
   sales: {
     labels: ["Jan", "Fev", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    datasets: { label: "Mobile apps", data: [50, 40, 300, 320, 500, 350, 200, 230, 500, 550, 600] },
+    datasets: { data: [50, 40, 300, 320, 500, 350, 200, 230, 300, 500, 550, 600] },
   },
   tasks: {
-    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    datasets: { label: "Desktop apps", data: [50, 40, 300, 220, 500, 250, 400, 230, 500] },
+    labels: ["Jan", "Fev", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: { data: [50, 40, 300, 220, 500, 250, 400, 300, 350, 230, 500, 600] },
   },
 };
